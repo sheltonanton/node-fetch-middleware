2c3773cccc42611c0afb0ab62df798916eb22b35
Your <name@mail.com>
2020-11-08 17:11:29 +0530

    Library first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+let middlewares = [function(url, init, next){next(url, init);}]
+let middlewareFetch = fetch;
+
+function addMiddleware(mw){
+    middlewares.push(mw);
+    middlewareFetch = compose(middlewares);
+}
+
+middlewareFetch.addMiddleware = addMiddleware;
+
+module.exports = middlewareFetch;
